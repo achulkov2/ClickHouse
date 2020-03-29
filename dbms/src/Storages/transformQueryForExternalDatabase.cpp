34a8f362c80665d679bee188d59cfeea18974508
Andrey Chulkov <achulkov2@gmail.com>
2020-03-29 22:17:05 +0300

    debugging

--- dbms/src/Storages/transformQueryForExternalDatabase.cpp
+++ dbms/src/Storages/transformQueryForExternalDatabase.cpp
@@ -141,7 +141,7 @@ String transformQueryForExternalDatabase(
     const String & table,
     const Context & context)
 {
-    std::cerr << "HERE\n";
+    std::cerr << "HERE" << endl;
     auto clone_query = query.clone();
     DUMP(available_columns.getNames());
     auto syntax_result = SyntaxAnalyzer(context).analyzeSelect(clone_query, available_columns);
